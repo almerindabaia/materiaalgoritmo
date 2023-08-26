f603bd234cb44660eac781d1a6c13171477d2ccf
almerindabaia <lazeralmerinda@gmail.com>
2023-08-26 17:06:33 -0300

    primeirocommit

---
+++ javaaplication/package javaapplication9; - Copia.java
@@ -0,0 +1,21 @@
+package javaapplication9;
+
+import javax.swing.JOptionPane;
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line argume
+     */
+    public static void main(String[] args) {
+       int l1,alt1,area;
+       String l,alt;
+       l=JOptionPane.showInputDialog(null,"Digite um lado");
+       alt=JOptionPane.showInputDialog(null,"Digite a altura");
+       l1=Integer.parseInt(l);
+       alt1=Integer.parseInt(alt);
+       
+       area=
+       
+    }
+   
+}
